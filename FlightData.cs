29bdf764d5a3ea296cc1f6893d164efc5dad95d4
eklynx <myP@SS4code>
2020-09-07 11:38:34 -0700

    ForeFlight sending works, need unit testing

---
+++ FlightData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimForeflightLink
+{
+    public class FlightData
+    {
+        public float? Latitude { get; set; }
+        public float? Longitude { get; set; }
+        public float? Altitude { get; set; }
+        public float? GroundTrack { get; set; }
+        public float? GroundSpeed { get; set; }
+        public float? TrueHeading { get; set; }
+        public float? Pitch { get; set; }
+        public float? Roll { get; set; }
+
+        public void ClearData()
+        {
+            Latitude = Longitude = Altitude = GroundTrack = GroundSpeed = TrueHeading = Pitch = Roll = null;
+        }
+    }
+}
--- ForeFlightSender.cs
+++ ForeFlightSender.cs
@@ -1,30 +1,125 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
-using System.Net;
-using System.Net.Configuration;
+using System.Timers;
 
 namespace SimForeflightLink
 {
-    class ForeFlightSender
+    public class ForeFlightSender
     {
-        private const String DeviceName = "MSFS 2020";
+        public static readonly string DEFAULT_DEVICE_NAME = "MSFS 2020";
+        private static readonly string GPS_MESSAGE_FORMAT = "XGPS{0},{1:F2},{2:F2},{3:F1},{4:F2},{5:F1}";
+        private static readonly string ATTITUDE_MESSAGE_FORMAT = "XATT{0},{1:F1},{2:F1},{3:F1}";
+        public static readonly int DEFAULT_PORT = 49002;
+        public static readonly int GPS_RATE_MS = 1000;
+        public static readonly int ATTITUDE_RATE_MS = 150;
 
         private UdpClient udpClient;
-        private IPEndPoint endpoint;
+        private readonly Timer gpsTimer;
+        private readonly Timer attitudeTimer;
+        private readonly FlightData flightData;
 
-        public ForeFlightSender()
+
+        public string DeviceName { get; set; } = DEFAULT_DEVICE_NAME;
+        public IPEndPoint EndPoint { get; set; }
+
+
+        public ForeFlightSender(ref FlightData flightData, UdpClient udpClient)
         {
-            udpClient = new UdpClient(49002, AddressFamily.InterNetwork);
+            this.flightData = flightData;
+            this.udpClient = udpClient;
+
+            gpsTimer = new Timer(GPS_RATE_MS)
+            {
+                AutoReset = true
+            };
+            gpsTimer.Elapsed += (sender, args) => SendGps();
+
+            attitudeTimer = new Timer(ATTITUDE_RATE_MS)
+            {
+                AutoReset = true
+            };
+            attitudeTimer.Elapsed += (sender, args) => SendAttiude();
         }
 
-        private void send(String message)
+        public void Start()
         {
-            Byte[] msg = Encoding.ASCII.GetBytes(message);
-            udpClient.Send(msg, msg.Length, endpoint);
+            if (null == EndPoint)
+            {
+                throw new ArgumentNullException("The EndPoint has not been set");
+            }
+            gpsTimer.Start();
+            attitudeTimer.Start();
+        }
+
+        public void Stop()
+        {
+            gpsTimer.Stop();
+            attitudeTimer.Stop();
+        }
+
+        private void SendGps()
+        {
+            if (VerifyFlightData(flightData) && null != EndPoint)
+            {
+                string gpsString = string.Format(
+                    GPS_MESSAGE_FORMAT,
+                    DeviceName,
+                    flightData.Longitude,
+                    flightData.Latitude,
+                    flightData.Altitude,
+                    flightData.GroundTrack,
+                    flightData.GroundSpeed
+                    );
+                Send(gpsString);
+            }
+        }
+
+        private void SendAttiude()
+        {
+            if (VerifyFlightData(flightData) && null != EndPoint)
+            {
+                string attitudeString = string.Format(
+                    ATTITUDE_MESSAGE_FORMAT,
+                    DeviceName,
+                    flightData.TrueHeading,
+                    flightData.Pitch,
+                    flightData.Roll
+                    );
+                Send(attitudeString);
+            }
+        }
+
+        private void Send(string message)
+        {
+            lock (this)
+            {
+                Console.Out.WriteLine("Sending message ${message}");
+                byte[] msg = Encoding.ASCII.GetBytes(message);
+                udpClient.Send(msg, msg.Length, EndPoint);
+            }
+        }
+
+        private static bool VerifyFlightData(FlightData flightData)
+        {
+            if (null == flightData.Altitude)
+                return false;
+            if (null == flightData.GroundSpeed)
+                return false;
+            if (null == flightData.GroundTrack)
+                return false;
+            if (null == flightData.Latitude)
+                return false;
+            if (null == flightData.Longitude)
+                return false;
+            if (null == flightData.Pitch)
+                return false;
+            if (null == flightData.Roll)
+                return false;
+            if (null == flightData.TrueHeading)
+                return false;
+            return true;
         }
     }
 }
